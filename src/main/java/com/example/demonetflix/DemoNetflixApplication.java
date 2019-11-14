ddedec24a9dd95ed3e83b2b802221b40fb68dd99
maikec@126.com <a6259272>
2019-11-14 17:30:02 +0800

    初始化项目

---
+++ src/main/java/com/example/demonetflix/DemoNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNetflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNetflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demonetflix/DemoNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonetflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNetflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
