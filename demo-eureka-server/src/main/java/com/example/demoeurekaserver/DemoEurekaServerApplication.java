baf3152315b8261bc3d759dfb6932543a8b04881
maikec@126.com <a6259272>
2019-11-15 11:04:59 +0800

    1.eureka authenticating with username & password

--- demo-eureka-server/src/main/java/com/example/demoeurekaserver/DemoEurekaServerApplication.java
+++ demo-eureka-server/src/main/java/com/example/demoeurekaserver/DemoEurekaServerApplication.java
@@ -3,6 +3,10 @@ package com.example.demoeurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +16,13 @@ public class DemoEurekaServerApplication {
         SpringApplication.run(DemoEurekaServerApplication.class, args);
     }
 
+}
+@Configuration
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
 }
